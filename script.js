627ff3f974ec3a452b374ede5bc0fe47d9ceda9b
Armine-web <83885299+Armine-web@users.noreply.github.com>
2022-06-08 16:18:45 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,302 @@
+function readMore(){
+    let dots = document.getElementById("dots");  
+    let more = document.getElementById("more");
+    let btn = document.getElementById("btn");
+    let content =document.getElementById ("content");
+    let nav_content = document.getElementById("nav_content");
+    let btn01 = document.getElementById("btn01");
+    let btn02 = document.getElementById("btn02");
+    let title=document.getElementById("title");
+    
+  
+
+if (dots.style.display === "none") {
+    nav_content.style.display = "flex";  
+    dots.style.display ="inline";
+    btn.innerHTML="read more";
+    more.style.display="none";
+    content.style.width ="500px";
+    title.style.paddingTop ="80px"
+    btn01.style.display="none";
+   
+    btn02.style.display="none";
+
+    
+}else{
+    dots.style.display ="none";
+    nav_content.style.display = "block";
+    nav_content.style.textAlign ="justify";
+    btn.innerHTML="hide";
+    more.style.display="inline";
+    content.style.width ="800px";
+    content.style.paddingLeft ="40px";
+    title.style.paddingTop ="20px";
+    btn01.style.display="inline";  
+    btn02.style.display="inline";
+   
+
+}
+}
+function readMore1(){
+    let dots1 = document.getElementById("dots1");  
+    let more1 = document.getElementById("more1");
+    let btn1 = document.getElementById("btn1");
+    let content1 =document.getElementById ("content1");
+    let nav_content1 = document.getElementById("nav_content1");
+    
+  
+
+if (dots1.style.display === "none") {
+    nav_content1.style.display = "flex";
+    dots1.style.display ="inline";
+    btn1.innerHTML="read more";
+    more1.style.display="none";
+    content1.style.width ="500px";
+    
+    
+}else{
+    dots1.style.display ="none";
+    nav_content1.style.display = "block";
+    nav_content1.style.textAlign ="justify"
+    btn1.innerHTML="hide";
+    more1.style.display="inline";
+    content1.style.width ="800px";
+    content1.style.paddingLeft ="40px";
+    
+
+}
+}
+function readMore2(){
+    let dots2 = document.getElementById("dots2");  
+    let more2 = document.getElementById("more2");
+    let btn2 = document.getElementById("btn2");
+    let content2 =document.getElementById ("content2");
+    let nav_content2 = document.getElementById("nav_content2");
+  
+
+if (dots2.style.display === "none") {
+    nav_content2.style.display = "flex";
+    dots2.style.display ="inline";
+    btn2.innerHTML="read more";
+    more2.style.display="none";
+    content2.style.width ="500px";
+    
+}else{
+    dots2.style.display ="none";
+    nav_content2.style.display = "block";
+    nav_content2.style.textAlign ="justify"
+    btn2.innerHTML="hide";
+    more2.style.display="inline";
+    content2.style.width ="800px";
+    content2.style.paddingLeft ="40px";
+
+}
+}
+function readMore3(){
+    let dots3 = document.getElementById("dots3");  
+    let more3 = document.getElementById("more3");
+    let btn3 = document.getElementById("btn3");
+    let content3 =document.getElementById ("content3");
+    let nav_content3 = document.getElementById("nav_content3");
+  
+
+if (dots3.style.display === "none") {
+    nav_content3.style.display = "flex";
+    dots3.style.display ="inline";
+    btn3.innerHTML="read more";
+    more3.style.display="none";
+    content3.style.width ="500px";
+    
+}else{
+    dots3.style.display ="none";
+    nav_content3.style.display = "block";
+    nav_content3.style.textAlign ="justify"
+    btn3.innerHTML="hide";
+    more3.style.display="inline";
+    content3.style.width ="800px";
+    content3.style.paddingLeft ="40px";
+
+}
+}
+function readMore4(){
+    let dots4 = document.getElementById("dots4");  
+    let more4 = document.getElementById("more4");
+    let btn4 = document.getElementById("btn4");
+    let content4 =document.getElementById ("content4");
+    let nav_content4 = document.getElementById("nav_content4");
+  
+
+if (dots4.style.display === "none") {
+    nav_content4.style.display = "flex";
+    dots4.style.display ="inline";
+    btn4.innerHTML="read more";
+    more4.style.display="none";
+    content4.style.width ="500px";
+    
+}else{
+    dots4.style.display ="none";
+    nav_content4.style.display = "block";
+    nav_content4.style.textAlign ="justify"
+    btn4.innerHTML="hide";
+    more4.style.display="inline";
+    content4.style.width ="800px";
+    content4.style.paddingLeft ="40px";
+
+}
+}
+function readMore5(){
+    let dots5 = document.getElementById("dots5");  
+    let more5 = document.getElementById("more5");
+    let btn5 = document.getElementById("btn5");
+    let content5 =document.getElementById ("content5");
+    let nav_content5 = document.getElementById("nav_content5");
+  
+
+if (dots5.style.display === "none") {
+    nav_content5.style.display = "flex";
+    dots5.style.display ="inline";
+    btn5.innerHTML="read more";
+    more5.style.display="none";
+    content5.style.width ="500px";
+    
+}else{
+    dots5.style.display ="none";
+    nav_content5.style.display = "block";
+    nav_content5.style.textAlign ="justify"
+    btn5.innerHTML="hide";
+    more5.style.display="inline";
+    content5.style.width ="800px";
+    content5.style.paddingLeft ="40px";
+
+}
+}
+function readMore6(){
+    let dots6 = document.getElementById("dots6");  
+    let more6 = document.getElementById("more6");
+    let btn6 = document.getElementById("btn6");
+    let content6 =document.getElementById ("content6");
+    let nav_content6 = document.getElementById("nav_content6");
+  
+
+if (dots6.style.display === "none") {
+    nav_content6.style.display = "flex";
+    dots6.style.display ="inline";
+    btn6.innerHTML="read more";
+    more6.style.display="none";
+    content6.style.width ="500px";
+    
+}else{
+    dots6.style.display ="none";
+    nav_content6.style.display = "block";
+    nav_content6.style.textAlign ="justify"
+    btn6.innerHTML="hide";
+    more6.style.display="inline";
+    content6.style.width ="800px";
+    content6.style.paddingLeft ="40px";
+
+}
+}
+function readMore7(){
+    let dots7 = document.getElementById("dots7");  
+    let more7 = document.getElementById("more7");
+    let btn7 = document.getElementById("btn7");
+    let content7 =document.getElementById ("content7");
+    let nav_content7 = document.getElementById("nav_content7");
+  
+
+if (dots7.style.display === "none") {
+    nav_content7.style.display = "flex";
+    dots7.style.display ="inline";
+    btn7.innerHTML="read more";
+    more7.style.display="none";
+    content7.style.width ="500px";
+    
+}else{
+    dots7.style.display ="none";
+    nav_content7.style.display = "block";
+    nav_content7.style.textAlign ="justify"
+    btn7.innerHTML="hide";
+    more7.style.display="inline";
+    content7.style.width ="700px";
+    content7.style.paddingLeft ="40px";
+
+}
+}
+function readMore8(){
+    let dots8 = document.getElementById("dots8");  
+    let more8 = document.getElementById("more8");
+    let btn8 = document.getElementById("btn8");
+    let content8 =document.getElementById ("content8");
+    let nav_content8 = document.getElementById("nav_content8");
+  
+
+if (dots8.style.display === "none") {
+    nav_content8.style.display = "flex";
+    dots8.style.display ="inline";
+    btn8.innerHTML="read more";
+    more8.style.display="none";
+    content8.style.width ="500px";
+    
+}else{
+    dots8.style.display ="none";
+    nav_content8.style.display = "block";
+    nav_content8.style.textAlign ="justify"
+    btn8.innerHTML="hide";
+    more8.style.display="inline";
+    content8.style.width ="800px";
+    content8.style.paddingLeft ="40px";
+
+}
+}
+function readMore9(){
+    let dots9 = document.getElementById("dots9");  
+    let more9 = document.getElementById("more9");
+    let btn9 = document.getElementById("btn9");
+    let content9 =document.getElementById ("content9");
+    let nav_content9 = document.getElementById("nav_content9");
+  
+
+if (dots9.style.display === "none") {
+    nav_content9.style.display = "flex";
+    dots9.style.display ="inline";
+    btn9.innerHTML="read more";
+    more9.style.display="none";
+    content9.style.width ="500px";
+    
+}else{
+    dots9.style.display ="none";
+    nav_content9.style.display = "block";
+    nav_content9.style.textAlign ="justify";
+    btn9.innerHTML="hide";
+    more9.style.display="inline";
+    content9.style.width ="800px";
+    content9.style.paddingLeft ="40px";
+
+}
+}
+
+document.getElementById("content_list").onmouseover=function(event){
+    let target = event.target;
+    if (target.className == "title") {
+        let s = getElementByClassName("title01");
+        closeMenu();
+        s[0].style.display="block";
+    }
+}
+document.onmouseover=function (event) {
+    let target = event.target;
+    console.log=event.target;
+    if (target.className!="title"&& target.className!="title01") {
+        closeMenu();
+    }
+}
+function closeMenu(){
+   
+    let submenu = document.getElementsByClassName("title01");
+    for (let i = 0; i < submenu.length; i++) {
+        submenu[i].style.display = "none";
+        
+    }
+}
+
+
